5280e0db5d8cf827d3a3dcde2a3286899cb62a1c
Rafael <Rafael.mattos468@gmail.com>
2022-10-16 14:05:12 -0300

    update

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//passo 1 - pegar os elementos que representam as abas no HTML
+
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach((aba) => {
+  // passo 2 - dar um jeito de identificar o clique no elemento aba
+  aba.addEventListener("click", function () {
+    if (aba.classList.contains("selecionado")) {
+      return;
+    }
+    // passo 3 - quando o usuario clicar, desmarcar a aba selecionada
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+
+    // passo 4 - marcar a aba clícada com selecionado
+    aba.classList.add("selecionado");
+
+    //passo 5 - esconder o conteúdo anterios
+
+    const informacaoSelecionada = document.querySelector(
+      ".informacao.selecionado"
+    );
+    informacaoSelecionada.classList.remove("selecionado");
+
+    //passo 6 - mostrar o conteúdo da aba selecionada
+        
+
+  });
+});
